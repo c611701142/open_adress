ac12af8ceefd7f8742a67a8f29d1d8df9cb369ac
c611701142 <km9216bp@gmail.com>
2020-10-08 18:21:14 +0900

    unorder_mapに変更
    プログラムを少し直しました。
    pull requestをやってみたのですが、かえって見づらくなってしまったので、リンクを送らせていただきます

--- hashing.hpp
+++ hashing.hpp
@@ -2,61 +2,45 @@
 #define OPEN_ADRESS__HASHING_HPP_
 #include <iostream>
 #include <vector>
-#include <unordered_map>  // for std::unordered_map
-using namespace std;
+#include <unordered_map>
+#include <cassert>
 
 namespace kuroda {
 
-
 class HashTableInterface {
- public:
-  virtual int get(int key) const = 0;//const消すとコンパイル通りました
+public:
+  virtual int get(int key) const = 0;
   virtual void set(int key, int value) = 0;
 };
 
 class HashTable : public HashTableInterface{
-
-/*void set(int key, int value){
-      //std::unordered_map<int, int> mp;    //  int → int のハッシュ連想配列
-    mp[key] = value;
-    for(auto itr = mp.begin(); itr != mp.end(); ++itr) {
-        std::cout << "key = " << itr->first           // キーを表示
-                        << ", val = " << itr->second << "\n";    // 値を表示
-    }*/  
- //}//この関数が呼び出されるのは、4の倍数が選ばれたとき
-
+// TODO: Needs implementation by yourself.
 public:
   static constexpr int kIndexSize = 0x100;
 private:
-  std::vector<int> v;
-  int target;
+  std::unordered_map<int, int> v;
 public:
   HashTable() {
-    v.resize(kIndexSize, -1);
   }
-  //constexpr int kIndexSize = 0x100;//256
+  
   
   void set(int key, int value){
-    std::cout << "key =" << key << std::endl;
-    v.insert(v.begin() + key, value);
-    
-  }//この関数が呼び出されるのは、4の倍数が選ばれたとき
+    v[key] = value; 
+  }
 
-  int get(int key) const{
+  int get(int key) const{  
+    std::cout << v.count(key) << std::endl;  
+    //setでアクセスしたところ(4の倍数)は1、他は0になる
+    if(v.count(key) == 0) { 
+      return -1;
+    }
+    int target = v.find(key)->second;
+    return target;
 
-    std::cout << "target = " << v[key] << std::endl;
-    std::cout << "key = " << key << std::endl;
-    std::cout << "size = " << v.size()  << std::endl;
-    //return target % 256;
-
-    //target =  v[key];
-    //return target;
-    return v[key];
-
-  }//ここで返す値をexpected_valueと一致させたい
+  }
 
 };
 
 }
 
-#endif //OPEN_ADRESS__HASHING_HPP_
+#endif //OPEN_ADRESS__HASHING_HPP
